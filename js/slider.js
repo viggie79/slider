d70a66a7dbca11425b8b3019b84f92ee4d63ea76
Viggie <viggie79@gmail.com>
2015-05-13 11:06:15 +0700

    added all slider files

---
+++ js/slider.js
@@ -0,0 +1,45 @@
+ sliderInt=1;
+ sliderNext=2;
+
+$(document).ready(function(){
+	$('#slider img#1').fadeIn(300);
+	startSlider();
+});
+function startSlider(){
+	//count variable = number of img inside slider .size is a jq function that counts elements
+	count=$("#slider img").size();
+	loop=setInterval(function(){
+		if(sliderNext>count) {
+			sliderNext=1;
+			sliderInt=1;
+		}
+		$("#slider img").fadeOut(300);
+		$("#slider img#"+sliderNext).fadeIn(300);
+		sliderInt=sliderNext;
+		sliderNext=sliderNext+1;
+	},3000)
+}
+/*
+function prev() {
+	newSlide=sliderInt-1;
+	showSlide(newSlide);
+};
+
+function next() {
+	newSlide=sliderInt+1;
+	showSlide(newSlide);
+});
+
+function showSlide(id){
+	if(id>count) {
+			id=1;
+		}
+	else if (id<1) {
+		id=count;
+	}
+		$("#slider img").fadeOut(300);
+		$("#slider img#"+id).fadeIn(300);
+		sliderInt=id;
+		sliderNext=id+1;
+}
+*/
